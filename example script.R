fc175a97d49499c2cb39e093f972c149663e35e9
satokamov <satokamov@uchicago.edu>
2020-07-04 23:30:18 -0500

    +test commit

---
+++ example script.R
@@ -0,0 +1,14 @@
+
+data("DNase")
+
+write.csv(x = DNase, file = "DNase datafile.csv")
+#adding a comment
+
+#adding another comment
+
+#Find the mean of 1-455
+A <- 1:455
+mean(A)
+
+#sandksdn;kdnas;kdna;sdk
+
